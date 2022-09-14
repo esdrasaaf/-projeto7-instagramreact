26e71349deca2af0ce480f09a53033b84ebde538
Esdras Antero <esdrasanterofontes@gmail.com>
2022-09-14 14:44:15 -0300

    Componentização feita

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Corpo from "./Corpo"
+import Navbar from "./Navbar"
+import FundoMobile from "./Fundo-mobile"
+
+export default function App() {
+    return (
+        <div>
+            <Navbar/>
+            <Corpo/>
+            <FundoMobile/>
+        </div>
+    )
+}
---
+++ src/Sidebar.js
@@ -0,0 +1,20 @@
+import Usuario from "./Usuario"
+import Sugestoes from "./Sugestoes"
+
+export default function Sidebar() {
+    return (
+    <div class="sidebar">
+        <Usuario/>
+
+        <Sugestoes/>
+
+        <div class="links">
+        Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
+        </div>
+
+        <div class="copyright">
+        © 2021 INSTAGRAM DO FACEBOOK
+        </div>
+  </div>
+    )
+}
